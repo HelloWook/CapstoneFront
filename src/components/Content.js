ac0548a9b97af2626c281dd12acf92fa0b9e0af3
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-01-10 22:40:14 +0900

    getflower

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,45 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Content.css";
 import Article from "./Article";
 import UpdateArticle from "./UpdateArticle";
 import { useSelector } from "react-redux";
+import { GetFlower } from "../services/FlowerAPI";
 
 function Content() {
-  const { isLoggedIn, email, nickname } = useSelector((state) => state);
+  const { isLoggedIn, email } = useSelector((state) => state);
   const [searchTerm, setSearchTerm] = useState("");
-  const [myFlower, setMyflower] = useState(
-    // 테스트 데이터
-    [
-      {
-        img: "https://i.namu.wiki/i/4ucM0uKIFisi3MBXl0k8ZY3goQZtMYZxaqbih6jgBRmesC0Ode8dzd6JGeFStJl3ISkli3FVCBEFff6uf9zyUw.webp",
-        flowerName: "다육이",
-        type: "장미",
-        temp: 36.5,
-        humidity: 94.0,
-      },
-      {
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Echinocereus_engelmannii_and_Cylindropuntia_bigelovii_at_Joshua_Tree_NP.jpg/800px-Echinocereus_engelmannii_and_Cylindropuntia_bigelovii_at_Joshua_Tree_NP.jpg",
-        flowerName: "망곰이",
-        type: "선인장",
-        temp: 36.5,
-        humidity: 98,
-      },
-      {
-        img: "https://kukka-2-media-123.s3.amazonaws.com/media/contents/event_template/d5b6a32f-7720-48bb-a4f0-0d01dc0115fe.jpg",
-        flowerName: "바라기",
-        type: "해바라기",
-        temp: 36.2,
-        humidity: 94,
-      },
-    ]
-  );
-
-  // 모달 관리하는 상태
-  const [updateMode, setUdateMode] = useState(false);
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
+  const [myFlower, setMyflower] = useState([]);
   // 리엑트 검색기능
   const filteredFlowers = myFlower.filter((data) =>
     Object.values(data).some(
@@ -48,6 +17,32 @@ function Content() {
         value.toLowerCase().includes(searchTerm.toLowerCase())
     )
   );
+  useEffect(() => {
+    async function fetchData() {
+      if (!isLoggedIn) {
+        return;
+      }
+      try {
+        const encodedEmail = encodeURIComponent(email);
+        const data = await GetFlower(encodedEmail);
+        setMyflower(() => [...data.result]);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(myFlower);
+  }, [myFlower]);
+
+  // 모달 관리하는 상태
+  const [updateMode, setUdateMode] = useState(false);
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="content">
       <div className="content-section-list">
@@ -74,10 +69,10 @@ function Content() {
           filteredFlowers.map((data, index) => (
             <Article
               key={index}
-              img={data.img}
-              flowerName={data.flowerName}
-              type={data.type}
-              temp={data.temp}
+              img={data.image_url}
+              flowerName={data.name}
+              type={data.kind}
+              temp={data.temperature}
               humidity={data.humidity}
             />
           ))
--- src/services/FlowerAPI.js
+++ src/services/FlowerAPI.js
@@ -24,3 +24,18 @@ export async function UploadFlower(
       throw error;
     });
 }
+
+export async function GetFlower(email) {
+  return axios
+    .get(`http://localhost:8080/flower/get/${email}`, {
+      headers: {
+        authorization: localStorage.getItem("accessToken"),
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
