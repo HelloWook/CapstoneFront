c532e23b2ab622fb3b661c589a0a4e03e88f423f
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-01-12 17:23:22 +0900

    post
    post

---
+++ src/components/Post.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../";
+function Post({ title, img, post, comment }) {
+  return <div className="post"></div>;
+}
+
+export default Post;
--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import "../styles/community.css";
+import Post from "../components/Post";
 function Community() {
   return (
     <div>
       <Header />
       <Banner />
+      <Post />
     </div>
   );
 }
