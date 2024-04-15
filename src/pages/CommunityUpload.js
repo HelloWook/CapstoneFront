a4f6d341c6bbe46afe32769e17a696f208ac1c16
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-04-16 02:54:07 +0900

    style:게시판 스타일링

--- src/pages/CommunityUpload.js
+++ src/pages/CommunityUpload.js
@@ -14,6 +14,12 @@ function CommunityUpload() {
         <h3 className="community-title">게시글 작성</h3>
         <div className="community-upload-box">
           <div className="community-upload-content">
+            <p className="community-upload-title"> 제목 </p>
+            <input
+              className="community-upload-input"
+              placeholder="제목을 입력해주세요"
+              type="text"
+            />
             <p className="community-upload-title"> 게시판</p>
             <div className="box">
               <ReactQuill theme="snow" value={value} onChange={setValue} />
