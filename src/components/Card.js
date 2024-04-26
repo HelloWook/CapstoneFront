7fd19463dc6d13025e70a9c1f3769ba74d074f81
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-04-27 01:25:06 +0900

    style:card 디자인 변경

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,13 @@ function Card({ title, content, email }) {
   return (
     <div className="card">
       <p>제목 : {title}</p>
+      <div className="image-box">
+        <img
+          className="image-thumbnail"
+          src="https://i.namu.wiki/i/4ucM0uKIFisi3MBXl0k8ZY3goQZtMYZxaqbih6jgBRmesC0Ode8dzd6JGeFStJl3ISkli3FVCBEFff6uf9zyUw.webp"
+          alt="post-img"
+        ></img>
+      </div>
       <p>작성자 : {email}</p>
     </div>
   );
